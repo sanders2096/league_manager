e47149402e0be474c86f7cfd07f7fe7dc9662384
tunes <sanders2096@gmail.com>
2017-01-02 20:13:06 -0700

    first commit
    first commit

---
+++ src/main/java/app/fantasy_football/LeagueManagerApplication.java
@@ -0,0 +1,12 @@
+package app.fantasy_football;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeagueManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeagueManagerApplication.class, args);
+	}
+}
---
+++ src/test/java/app/fantasy_football/LeagueManagerApplicationTests.java
@@ -0,0 +1,16 @@
+package app.fantasy_football;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LeagueManagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
